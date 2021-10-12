f6e4dbb8bfd92bec57fc29114fa8ad7b901d5ba1
Jan Krajnc <jan.krajnc17@gmail.com>
2021-10-12 21:33:50 +0200

    input lowercase

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ print("Hi! i'm a programme that converts Kilometers into Miles")
 km = int(input("Input Kilometers: "))
 print(km * 0.6213)
 
-a = input("do you want to do another conversion?: ")
-while a == "yes" or a == "Yes" or a == "YES":
+a = (input("do you want to do another conversion?: "))
+a = a.lower()
+while a == "yes":
     km = int(input("input kilometers: "))
     miles = (km * 0.6213)
     print(miles)
-    a = input("do you want to do another conversion?: ")
-    if a == "no" or a == "No" or a == "NO":
+    a = (input("do you want to do another conversion?: "))
+    if a == "no":
         break
 print("goodbye!")
